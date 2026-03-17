4b4e661d805fb20fb23a144428509ac22934a25e
grehochka82 <lobodina0607@mail.ru>
2026-03-17 14:41:28 +0300

    Step 6: functions in JavaScript

--- main.js
+++ main.js
@@ -28,7 +28,57 @@ for (let i=1; i<=3; i++){
     }
 }
 
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(3,5));
 
+function sayHello(name){
+    console.log("Helli, ${name}");
+}
+sayHello("Тимофей")
+
+function greet (name="Гость"){
+    console.log("Привет," +name);
+}
+greet();
+greet("Анастасия")
+
+const add = function(a,b){
+    return a+b;
+};
+console.log(add(2,3));
+
+function makeCounter(){
+    let count =0;
+    return function(){
+        count++;
+        return count;
+    };
+}
+const counter = makeCounter();
+console.log(counter()); 
+console.log(counter()); 
+console.log(counter()); 
+
+function sumFunc(a,b){
+    return a+b;
+}
+
+const sumFunc2 = (a,b)=> a+b;
+
+const double =x=> x*2;
+
+const sumFun3 = (a,b) => a+b;
+
+const sayGreeting = ()=> console.log("Hello");
+
+const square =x =>x*x;
+
+const calculate =(a,b) =>{
+    let result = a+b;
+    return result*2;
+}
 //задания
 let sum = 0;
 for (let i = 1; i <= 10; i++) {
@@ -66,6 +116,18 @@ for (let i = 1; i <= 4; i++) {
     }
     console.log(stars);
 }
-
-
+function multiply(a, b) {
+    return a * b;
+}
+console.log("Произведение 4 и 5:", multiply(4, 5));
+//
+function printInfo(name, age) {
+    console.log(`Имя: ${name}, Возраст: ${age}`);
+}
+printInfo("Мария", 25);
+//
+function calculateDiscount(price, discount = 10) {
+    return price - (price * discount) / 100;
+}
+console.log("Цена со скидкой (по умолчанию 10%):", calculateDiscount(1000));
 
